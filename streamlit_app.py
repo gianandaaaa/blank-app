3a6b0a209c3aebbb573d480188158d04407b9a0b
gianandaaaa <gia.nanda26@gmail.com>
2025-05-21 14:24:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 giadudung")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image("C47C0122-CAD3-4379-BB19-92831F042FA9.jpeg", width=200)
+st.image( "C47C0122-CAD3-4379-BB19-92831F042FA9.jpeg", width=200)
